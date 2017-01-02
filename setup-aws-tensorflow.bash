55463feddb0a67d436537eb3bdcbd3ab437a817f
PeterPanOnGit <pope1984@gmail.com>
2017-01-02 15:56:08 -0500

    added PIL installation instruction

--- setup-aws-tensorflow.bash
+++ setup-aws-tensorflow.bash
@@ -3,54 +3,71 @@
 # stop on error
 set -e
 ############################################
-# install into /mnt/bin
-sudo mkdir -p /mnt/bin
-sudo chown ubuntu:ubuntu /mnt/bin
+# install s3fs
+sudo apt-get update
+sudo apt-get install -y automake autotools-dev g++ git libcurl4-gnutls-dev libfuse-dev libssl-dev libxml2-dev make pkg-config
+sudo apt-get install git
+git clone https://github.com/s3fs-fuse/s3fs-fuse.git
+cd s3fs-fuse
+./autogen.sh
+./configure
+make
+sudo make install
 
-# install the required packages
-sudo apt-get update && sudo apt-get -y upgrade
-sudo apt-get -y install linux-headers-$(uname -r) linux-image-extra-`uname -r`
+cd ~/
+echo AKIAICDS6KM4FJIZV7FQ:3fRVoPUN+OEwnuPmAjLMJQmLDpCCDHk73JkgQXkj > .passwd-s3fs
+chmod 600 .passwd-s3fs
+mkdir s3mnt
+mkdir /tmp/cache
+chmod 777 s3mnt /tmp/cache
+s3fs imagenet-sophon s3mnt -o use_cache=/tmp/cache
 
-# install cuda
-wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.5-18_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu1404_7.5-18_amd64.deb
-rm cuda-repo-ubuntu1404_7.5-18_amd64.deb
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y build-essential git python-pip libfreetype6-dev libxft-dev libncurses-dev libopenblas-dev gfortran python-matplotlib libblas-dev liblapack-dev libatlas-base-dev python-dev python-pydot linux-headers-generic linux-image-extra-virtual unzip python-numpy swig python-pandas python-sklearn unzip wget pkg-config zip g++ zlib1g-dev libcurl3-dev
+sudo pip install -U pip
+
+# install cuda8
+wget https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda-repo-ubuntu1604-8-0-local_8.0.44-1_amd64-deb
+sudo dpkg -i cuda-repo-ubuntu1604-8-0-local_8.0.44-1_amd64-deb
+rm cuda-repo-ubuntu1604-8-0-local_8.0.44-1_amd64-deb
 sudo apt-get update
 sudo apt-get install -y cuda
 
-# get cudnn
-CUDNN_FILE=cudnn-7.0-linux-x64-v4.0-prod.tgz
-wget http://developer.download.nvidia.com/compute/redist/cudnn/v4/${CUDNN_FILE}
-tar xvzf ${CUDNN_FILE}
-rm ${CUDNN_FILE}
-sudo cp cuda/include/cudnn.h /usr/local/cuda/include # move library files to /usr/local/cuda
-sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
-sudo chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
-rm -rf cuda
+# install cudnn
+sudo dpkg -i libcudnn5_5.1.5-1+cuda8.0_amd64.deb
+sudo dpkg -i libcudnn5-dev_5.1.5-1+cuda8.0_amd64.deb
 
-# set the appropriate library path
-echo 'export CUDA_HOME=/usr/local/cuda
-export CUDA_ROOT=/usr/local/cuda
-export PATH=$PATH:$CUDA_ROOT/bin:$HOME/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_ROOT/lib64
-' >> ~/.bashrc
+#installl bazel (optional)
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+# Hack to silently agree license agreement
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+sudo apt-get install -y oracle-java8-installer
 
-# install anaconda
-wget http://repo.continuum.io/archive/Anaconda3-4.0.0-Linux-x86_64.sh
-bash Anaconda3-4.0.0-Linux-x86_64.sh -b -p /mnt/bin/anaconda3
-rm Anaconda3-4.0.0-Linux-x86_64.sh
-echo 'export PATH="/mnt/bin/anaconda3/bin:$PATH"' >> ~/.bashrc
-
-# install tensorflow
-export TF_BINARY_URL='https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow-0.9.0rc0-cp35-cp35m-linux_x86_64.whl'
-
-/mnt/bin/anaconda3/bin/pip install $TF_BINARY_URL
+echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+curl https://storage.googleapis.com/bazel-apt/doc/apt-key.pub.gpg | sudo apt-key add -
+sudo apt-get update && sudo apt-get install bazel
+sudo apt-get upgrade bazel
 
 # install monitoring programs
 sudo wget https://git.io/gpustat.py -O /usr/local/bin/gpustat
 sudo chmod +x /usr/local/bin/gpustat
 sudo nvidia-smi daemon
 sudo apt-get -y install htop
+
+#install tensorflow
+sudo apt-get install -y python-pip python-dev python-virtualenv
+virtualenv --system-site-packages ~/tensorflow
+source ~/tensorflow/bin/activate
+export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-0.12.0rc1-cp27-none-linux_x86_64.whl
+pip install --upgrade $TF_BINARY_URL
+sudo apt-get install build-essential gfortran libatlas-base-dev python-pip python-dev
+pip install --upgrade scipy
+sudo apt-get install libjpeg-dev libfreetype6 libfreetype6-dev zlib1g-dev
+pip install Pillow
+git clone https://github.com/tensorflow/magenta.git
 
 # reload .bashrc
 exec bash
